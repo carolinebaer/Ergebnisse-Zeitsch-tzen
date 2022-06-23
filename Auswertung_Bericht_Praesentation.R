e97d0a38da12d2025cad37614970412f2defdf95
carolinebaer <94549515+carolinebaer@users.noreply.github.com>
2022-06-23 14:47:48 +0200

    Ergänzung: Tabellen

--- Auswertung_Bericht_Praesentation.R
+++ Auswertung_Bericht_Praesentation.R
@@ -3,6 +3,7 @@
 library(readxl)
 library(ggplot2)
 library(patchwork)
+library(xtable)
 
 # Einlesen des Versuchsprotokolls:
 versuchsprotokoll <- read_excel("versuchsprotokoll.xlsx")
@@ -147,3 +148,159 @@ ggplot(tag1, aes(ziel_schaetzung_hdtsek/100, rel_abweichung)) +
   geom_point(pch = 16, size = 3) +
   scale_x_continuous(breaks = c(20, 60, 100), limits = c(5, 115))
 
+
+# Tabellen-Erstellung:
+# Median, Interquartilsabstand, 1. und 3. Quartil, Maximum, Minimum, Spannweite
+erstell_tabelle <- function(daten){
+  speicher <- numeric(7)
+  speicher[1] <- median(daten)
+  speicher[2] <- IQR(daten)
+  speicher[3] <- quantile(daten, 1/4)
+  speicher[4] <- quantile(daten, 3/4)
+  speicher[5] <- max(daten)
+  speicher[6] <- min(daten)
+  speicher[7] <- speicher[5] - speicher[6]
+  names(speicher) <- c("Median", "Interquartilsabstand", 
+                       "1.Quartil", "3.Quartil", "Maximum", "Minimum", 
+                       "Spannweite")
+  return(speicher)
+}
+
+# Datensaetze geordnet nach Zeitspanne:
+zeit20 <- subset(zeiten, zeiten$ziel_schaetzung == "20 sek")
+zeit60 <- subset(zeiten, zeiten$ziel_schaetzung == "60 sek")
+zeit100 <- subset(zeiten, zeiten$ziel_schaetzung == "100 sek")
+
+erster_tag20 <- subset(tag1, tag1$ziel_schaetzung == "20 sek")
+erster_tag60 <- subset(tag1, tag1$ziel_schaetzung == "60 sek")
+erster_tag100 <- subset(tag1, tag1$ziel_schaetzung == "100 sek")
+
+zweiter_tag20 <- subset(tag2, tag2$ziel_schaetzung == "20 sek")
+zweiter_tag60 <- subset(tag2, tag2$ziel_schaetzung == "60 sek")
+zweiter_tag100 <- subset(tag2, tag2$ziel_schaetzung == "100 sek")
+
+# Tabelle: Betragsm. rel. Abweichungen - insgesamt
+tab_betrag_insg <- cbind(erstell_tabelle(zeit20$rel_abweichung_abs),
+                         erstell_tabelle(zeit60$rel_abweichung_abs),
+                         erstell_tabelle(zeit100$rel_abweichung_abs))
+
+colnames(tab_betrag_insg) <- c("20 Sek", "60 Sek", "100 Sek")
+
+tab_betrag_insg
+#                        20 Sek     60 Sek 100 Sek
+# Median               0.111000 0.04683333 0.20575
+# Interquartilsabstand 0.170375 0.13691667 0.13505
+# 1.Quartil            0.024250 0.04366667 0.12095
+# 3.Quartil            0.194625 0.18058333 0.25600
+# Maximum              0.319000 0.28083333 0.40310
+# Minimum              0.000500 0.02250000 0.02030
+# Spannweite           0.318500 0.25833333 0.38280
+
+
+# Tabelle: Relative Abweichungen - insgesamt
+tab_relativ_insg <- cbind(erstell_tabelle(zeit20$rel_abweichung),
+                          erstell_tabelle(zeit60$rel_abweichung),
+                          erstell_tabelle(zeit100$rel_abweichung))
+
+colnames(tab_relativ_insg) <- c("20 Sek", "60 Sek", "100 Sek")
+
+tab_relativ_insg
+#                         20 Sek       60 Sek   100 Sek
+# Median               -0.111000 -0.046666667  0.051650
+# Interquartilsabstand  0.170375  0.189666667  0.396300
+# 1.Quartil            -0.194625 -0.180583333 -0.143375
+# 3.Quartil            -0.024250  0.009083333  0.252925
+# Maximum              -0.000500  0.046833333  0.403100
+# Minimum              -0.319000 -0.280833333 -0.242900
+# Spannweite            0.318500  0.327666667  0.646000
+
+
+# Tabelle: Rel. Abweichungen - differenziert nach Tag
+tab_relativ_tag <- cbind(erstell_tabelle(erster_tag20$rel_abweichung),
+                         erstell_tabelle(erster_tag60$rel_abweichung),
+                         erstell_tabelle(erster_tag100$rel_abweichung),
+                         erstell_tabelle(zweiter_tag20$rel_abweichung),
+                         erstell_tabelle(zweiter_tag60$rel_abweichung),
+                         erstell_tabelle(zweiter_tag100$rel_abweichung))
+
+colnames(tab_relativ_tag) <- c("Tag 1 - 20 Sek", "Tag 1 - 60 Sek", 
+                               "Tag 1 - 100 Sek", "Tag 2 - 20 Sek",
+                               "Tag 2 - 60 Sek", "Tag 2 - 100 Sek")
+
+tab_relativ_tag
+#                      Tag 1 - 20 Sek Tag 1 - 60 Sek Tag 1 - 100 Sek
+# Median                    -0.135500   -0.118833333         -0.1336
+# Interquartilsabstand       0.238625    0.226416667          0.4680
+# 1.Quartil                 -0.260875   -0.231583333         -0.1727
+# 3.Quartil                 -0.022250   -0.005166667          0.2953
+# Maximum                   -0.000500    0.046833333          0.4031
+# Minimum                   -0.319000   -0.280833333         -0.2429
+# Spannweite                 0.318500    0.327666667          0.6460
+# 
+#                      Tag 2 - 20 Sek Tag 2 - 60 Sek Tag 2 - 100 Sek
+# Median                    -0.111000    -0.04666667         0.08300
+# Interquartilsabstand       0.060625     0.09333333         0.10925
+# 1.Quartil                 -0.137000    -0.09633333         0.05165
+# 3.Quartil                 -0.076375    -0.00300000         0.16090
+# Maximum                   -0.008500     0.04066667         0.23880
+# Minimum                   -0.179000    -0.14600000         0.02030
+# Spannweite                 0.170500     0.18666667         0.21850
+
+
+# Tabelle: 20 Sekunden (insgesamt, Tag 1, Tag 2)
+tab_20_sek <- cbind(erstell_tabelle(zeit20$rel_abweichung),
+                    erstell_tabelle(erster_tag20$rel_abweichung),
+                    erstell_tabelle(zweiter_tag20$rel_abweichung))
+colnames(tab_20_sek) <- c("insgesamt", "Tag 1", "Tag 2")
+
+tab_20_sek
+#                      insgesamt     Tag 1     Tag 2
+# Median               -0.111000 -0.135500 -0.111000
+# Interquartilsabstand  0.170375  0.238625  0.060625
+# 1.Quartil            -0.194625 -0.260875 -0.137000
+# 3.Quartil            -0.024250 -0.022250 -0.076375
+# Maximum              -0.000500 -0.000500 -0.008500
+# Minimum              -0.319000 -0.319000 -0.179000
+# Spannweite            0.318500  0.318500  0.170500
+
+
+# Tabelle: 60 Sekunden (insgesamt, Tag 1, Tag 2)
+tab_60_sek <- cbind(erstell_tabelle(zeit60$rel_abweichung),
+                    erstell_tabelle(erster_tag60$rel_abweichung),
+                    erstell_tabelle(zweiter_tag60$rel_abweichung))
+colnames(tab_60_sek) <- c("insgesamt", "Tag 1", "Tag 2")
+
+tab_60_sek
+#                         insgesamt        Tag 1       Tag 2
+# Median               -0.046666667 -0.118833333 -0.04666667
+# Interquartilsabstand  0.189666667  0.226416667  0.09333333
+# 1.Quartil            -0.180583333 -0.231583333 -0.09633333
+# 3.Quartil             0.009083333 -0.005166667 -0.00300000
+# Maximum               0.046833333  0.046833333  0.04066667
+# Minimum              -0.280833333 -0.280833333 -0.14600000
+# Spannweite            0.327666667  0.327666667  0.18666667
+
+
+# Tabelle: 100 Sekunden (insgesamt, Tag 1, Tag 2)
+tab_100_sek <- cbind(erstell_tabelle(zeit100$rel_abweichung),
+                    erstell_tabelle(erster_tag100$rel_abweichung),
+                    erstell_tabelle(zweiter_tag100$rel_abweichung))
+colnames(tab_100_sek) <- c("insgesamt", "Tag 1", "Tag 2")
+
+tab_100_sek
+#                      insgesamt   Tag 1   Tag 2
+# Median                0.051650 -0.1336 0.08300
+# Interquartilsabstand  0.396300  0.4680 0.10925
+# 1.Quartil            -0.143375 -0.1727 0.05165
+# 3.Quartil             0.252925  0.2953 0.16090
+# Maximum               0.403100  0.4031 0.23880
+# Minimum              -0.242900 -0.2429 0.02030
+# Spannweite            0.646000  0.6460 0.21850
+
+xtable(tab_betrag_insg, digits = 4)
+xtable(tab_relativ_insg, digits = 4)
+xtable(tab_relativ_tag, digits = 4)
+
+xtable(tab_20_sek, digits = 4)
+xtable(tab_60_sek, digits = 4)
+xtable(tab_100_sek, digits = 4)
